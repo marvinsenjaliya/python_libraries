df4f956c4d7c0b68f53a2f6271d771ec2f07d6f4
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-21 18:45:26 +0530

    make an immutable arrays{

---
+++ numpy/21.makearrayimmutable.py
@@ -0,0 +1,21 @@
+"""
+Created on :1/21/2020
+@author:Marvin Senjaliya
+"""
+
+"""
+problem statement:
+Write a Python program to make an array immutable (read-only). 
+Expected Output:
+Test the array is read-only or not:
+Try to change the value of the first element:
+Traceback (most recent call last):
+File "19236bd0-0bd9-11e7-a232-c706d0968eb6.py", line 6, in 
+x[0] = 1
+ValueError: assignment destination is read-only
+"""
+
+import numpy as np
+a=np.arange(10)
+a.flage.writeable = False
+a[0]=1
