cbaabdb66fd9753ad4b8eb2f87608a14fbef1e61
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-22 12:32:10 +0530

    create the array using panda

---
+++ pandas/01.createarray.py
@@ -0,0 +1,13 @@
+"""
+Created on:1/22/2020
+@author:Marvin Senjaliya
+"""
+"""
+problem statement:
+. Write a Python program to create and display a one-dimensional array-like object containing an array of data using Pandas module. 
+"""
+
+import pandas as pd
+a1=[1,2,3,4,5]
+s1=pd.Series(a1)
+print(s1)
