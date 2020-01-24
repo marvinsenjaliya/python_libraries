289d70fdb076d0f02486a1943c6aba6dc45a5412
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-24 10:41:55 +0530

    Added:program with witdh and position of bar

---
+++ matplotlib/Barchart/08_selsect_the_width_of_bar.py
@@ -0,0 +1,33 @@
+'''
+Created on 23/01/2020
+@author: B Akash
+'''
+'''
+problem statement:
+Write a Python programming to display a bar chart of the popularity of programming Languages. Select the width of each bar and their positions.
+Sample data: 
+Programming languages: Java, Python, PHP, JavaScript, C#, C++
+Popularity: 22.2, 17.6, 8.8, 8, 7.7, 6.7 
+
+'''
+
+from matplotlib import pyplot as plt
+
+languages=['Java','Python','PHP','JavaScript','C#','C++']
+popularity=[22.2,17.6,8.8,8,7.7,6.7]
+
+plt.bar(languages,popularity,data=[1,2,3,4,5,6])
+plt.xlabel("Programming Languages",color='maroon')
+plt.ylabel("Popularity",color='maroon')
+
+x_pos=[index for index, value in enumerate(languages)]
+plt.xticks(x_pos,languages)
+y_pos=[0,1,2,3,4,5]
+width=[0.5,0.8,0.6,0.7,0.4,0.25]
+plt.bar(y_pos,popularity,width=width)
+plt.xticks(y_pos,languages)
+plt.minorticks_on()
+plt.grid(which='major',linestyle='-',color='blue')
+plt.grid(which='minor',linestyle=":",color='black')
+plt.title("Popularity of programming languages", color='black')
+plt.show()
